198aa3d7f2995105ff86e6ffdcb113750e40e94a
Rodrigo da Silva <rodrigoryan618@gmail.com>
2022-11-16 15:14:00 -0300

    fix/controller-register-and-alerts

--- controllers/register.php
+++ controllers/register.php
@@ -13,34 +13,20 @@ $result = mysqli_query($connect, $sql);
 $row = mysqli_fetch_assoc($result);
 
 if ($row['total'] == 1) {
-    $_SESSION['usuario_existe'] = true;
-    header('Location: register.php');
-    exit;
-}
+        header('Location: /linkfour/register.php');
+        $_SESSION['existing_user'] = true ;    
+} else{
+    if($fone == "" || $nome == "" || $usuario == "" || $senha == "" ){
+        $_SESSION['empty_fields'] = true;
+    }else{
+        $_SESSION['account_created'] = true;
 
-$msg = "";
+        $sql = "INSERT INTO cadastro (id, fone, nome, usuario, senha) VALUES ( ' ' , '$fone', '$nome', '$usuario', '$senha')";
 
-if ($fone  == "" ) {
-    $msg .= 'Preencha o campo de telefone ';
-    echo "<script> alert( ' $msg ' ) </script>";
-} elseif ($nome == "") {
-    $msg .= 'Preencha o campo do nome ';
-    echo "<script> alert( ' $msg ' ) </script>";
-} elseif ($usuario == "") {
-    $msg .= 'Preencha o campo de nome de usúario ';
-    echo "<script> alert( ' $msg ' ) </script>";
-} elseif ($senha == "") {
-    $msg .= 'Preencha o campo da senha';
-    echo "<script> alert( ' $msg ' ) </script>";
-} else {
-
-    $sql = "INSERT INTO cadastro (id, fone, nome, usuario, senha) VALUES ( ' ' , '$fone', '$nome', '$usuario', '$senha')";
-
-    if (mysqli_query($connect, $sql)) {
-        
+        if (mysqli_query($connect, $sql)) {
+        }
+        mysqli_close($connect);
     }
-    mysqli_close($connect);
+    
+    header('Location: /linkfour/register.php');
 }
-
-// Não está imprimindo o  alert de conta criada
-header('Location: /linkfour/register.php');
--- register.php
+++ register.php
@@ -1,12 +1,13 @@
 <!DOCTYPE html>
+<?php session_start(); ?>
 
 <html lang="en">
 
 <head>
 
-    <link rel="stylesheet" type="text/css" href="css/style-2.css" media="screen"/>
+    <link rel="stylesheet" type="text/css" href="css/style-2.css" media="screen" />
     <script src="js/functions.js"></script>
-    
+
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -17,31 +18,62 @@
 <body>
 
     <div class="container">
-    
+
         <div class="box">
 
-            <div class="txtlg"><h1>LinkFour</h1></div>
-            <div class="txt-2"><h3>Cadastre-se para ter uma melhor divulgação de seus Links.</h3></div>
+            <div class="txtlg">
+                <h1>LinkFour</h1>
+            </div>
+            <div class="txt-2">
+                <h3>Cadastre-se para ter uma melhor divulgação de seus Links.</h3>
+            </div>
 
             <form action="controllers/register.php" method="post">
 
-            <div class="fone"><input type="text" name="fone" id="fone" placeholder="Número de Celular"></div>
-            <div class="nome"><input type="text" name="nome" id="nome" placeholder="Nome Completo"></div>
-            <div class="user"><input type="text" name="usuario" id="usuario" placeholder="Username"></div>
-            <div class="senha"><input type="password" name="senha" id="senha" placeholder="Senha"></div>
+                <div class="fone"><input type="text" name="fone" id="fone" placeholder="Número de Celular"></div>
+                <div class="nome"><input type="text" name="nome" id="nome" placeholder="Nome Completo"></div>
+                <div class="user"><input type="text" name="usuario" id="usuario" placeholder="Username"></div>
+                <div class="senha"><input type="password" name="senha" id="senha" placeholder="Senha"></div>
 
-            <!-- Criar botão de verificação e login JS/PHP -->
-            <div class="bt"><button class="bt" type="submit" onclick="cadastro()">Cadastrar-se</button></div>
+                <!-- Criar botão de verificação e login JS/PHP -->
+                <div class="bt"><button class="bt" type="submit" onclick="cadastro()">Cadastrar-se</button></div>
 
             </form>
 
-            <div class="txt"><h4>Tem uma conta?<a href="http://localhost/linkfour/login.php">Conecte-se</a></h4></div>
+            <div class="txt">
+                <h4>Tem uma conta?<a href="http://localhost/linkfour/login.php">Conecte-se</a></h4>
+            </div>
 
         </div>
 
+        <div class="alerts">
+
+            <?php if(@$_SESSION['existing_user'] == true): ?>
+                <div class="existing_user">
+                    <h3>Usuário existente!</h3>
+                </div>
+            <?php endif; unset($_SESSION['existing_user']); ?>
+
+            <?php if(@$_SESSION['account_created'] == true): ?>
+                <div class="registration_status">
+                    <h3>Conta Cadastrada</h3>
+                </div>
+            <?php endif; unset($_SESSION['account_created']); ?>
+
+            <?php if(@$_SESSION['empty_fields'] == true): ?>
+                <div class="registration_status">
+                    <h3>Há Campo(s) Vazios!</h3>
+                </div>
+            <?php endif; unset($_SESSION['empty_fields']); ?>
+            
+        </div>
+
+
+
+
     </div>
 
-    
+
 </body>
 
 </html>
